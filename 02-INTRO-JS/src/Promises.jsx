a0e34f38833a097fbe805ec41cbc83e2a76aee5e
edu290386 <eduardo.torres@outlook.com>
2023-08-18 19:25:49 -0500

    previous react

--- 02-INTRO-JS/src/Promises.jsx
+++ 02-INTRO-JS/src/Promises.jsx
@@ -2,7 +2,7 @@ const Promises = () => {
   const apiKey = "tD5ETZAxIU61RoM0XqW7DV3IVN0zOzB3";
   const url = `api.giphy.com/v1/gifs/random?api_key=${apiKey}`;
   
-  const petition = fetch(`https://63dc3850c45e08a04356e2c8.mockapi.io/Todo`);
+  const petition = fetch(`https://63dc3850c45e08a04356e2c8.mockapi.io/Todo`)
   petition
     .then((resp) => resp.json())
     .then(({data}) => {
@@ -13,6 +13,16 @@ const Promises = () => {
     })
     .catch(console.warn);
 
+    // const getImagen = async () => {
+    //  try {
+    //   const resp = await fetch(`https://63dc3850c45e08a04356e2c8.mockapi.io/Todo`)
+    //   const {data} = await resp.json()
+    //   const {url} = data.images.original;
+    //     const img = document.createElement("img");
+    //     img.src = url;
+    //  } catch (e) {
+    //   console.warn(e) 
+    // }
   return <div>Promises</div>;
 };
 
