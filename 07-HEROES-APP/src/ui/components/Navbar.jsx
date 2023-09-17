b08c9000ce0f87073cc2bc1487b5ef70730566e1
edu290386 <eduardo.torres@outlook.com>
2023-09-17 01:08:18 -0500

    logout

--- 07-HEROES-APP/src/ui/components/Navbar.jsx
+++ 07-HEROES-APP/src/ui/components/Navbar.jsx
@@ -1,13 +1,17 @@
+import { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/context/AuthContext";
 
 export const Navbar = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const { user, logout } = useContext(AuthContext);
 
-    const onLogout = () => {
-        navigate('/login', {
-            replace: true
-        });
-    }
+  const onLogout = () => {
+    logout();
+    navigate("/login", {
+      replace: true,
+    });
+  };
 
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-3">
@@ -47,7 +51,7 @@ export const Navbar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-primary">Eduardo</span>
+          <span className="nav-item nav-link text-primary">{user?.name}</span>
           <button
             onClick={onLogout}
             className="btn btn-primary nav-item nav-link"
